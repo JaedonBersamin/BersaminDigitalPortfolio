5c3906059a2d271c2fd7297e1bfbc7ecc3c5b748
Jumboflopper <jaedonbersamin@gmail.com>
2024-09-21 14:50:08 -0700

    Files

--- script.js
+++ script.js
@@ -82,3 +82,5 @@ toggleButton.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     toggleButton.textContent = document.body.classList.contains('dark-mode') ? 'Switch to Light Mode' : 'Switch to Dark Mode';
 });
+
+
